f0ea7deef81629c28e2a4c072406b362c45903b8
Colleen Schnettler <bitmappeddesigns@gmail.com>
2017-03-28 11:29:46 -0400

    Nested TodoItems in TodoList working

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,34 +1,36 @@
 class TodoItemsController < ApplicationController
-  before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_todo_list, :set_todo_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /todo_items
-  # GET /todo_items.json
-  def index
-    @todo_items = TodoItem.all
-  end
+  # GET todo_lists/todo_items
 
-  # GET /todo_items/1
-  # GET /todo_items/1.json
+ 
+  # GET /todo_lists/:todo_list_id/todo_items/1
   def show
   end
 
   # GET /todo_items/new
+  # GET /todo_lists/:todo_list_id/todo_items/new
   def new
-    @todo_item = TodoItem.new
+    #@todo_item = ToDoItem.new(todo_list_id, :todo_list_id)
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.new 
+    #Should be no completed box.  Somehow this calls the form? So it's new >>form>>if form is submitted>>create is called with the params from the form 
   end
 
-  # GET /todo_items/1/edit
+  # GET /todo_lists/:todo_list_id/todo_items/1/edit
   def edit
   end
 
-  # POST /todo_items
-  # POST /todo_items.json
+
+  # POST /todo_lists/:todo_list_id/todo_items
   def create
-    @todo_item = TodoItem.new(todo_item_params)
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.new(todo_item_params) 
+   
 
     respond_to do |format|
       if @todo_item.save
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully created.' }
+        format.html { redirect_to todo_list_url(@todo_list), notice: 'Todo item was successfully created.' }
         format.json { render :show, status: :created, location: @todo_item }
       else
         format.html { render :new }
@@ -37,12 +39,13 @@ class TodoItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todo_items/1
-  # PATCH/PUT /todo_items/1.json
+ 
+  # PATCH/PUT /todo_lists/:todo_list_id/todo_items/1
   def update
     respond_to do |format|
       if @todo_item.update(todo_item_params)
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully updated.' }
+        #format.html { redirect_to todo_list_todo_item_url, notice: 'Todo item was successfully updated.' } #Why don't I have to send in the ids here?
+        format.html { redirect_to todo_list_url(@todo_list), notice: 'Todo item was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo_item }
       else
         format.html { render :edit }
@@ -51,12 +54,12 @@ class TodoItemsController < ApplicationController
     end
   end
 
-  # DELETE /todo_items/1
-  # DELETE /todo_items/1.json
+ 
+  # DELETE /todo_lists/:todo_list_id/todo_items/1
   def destroy
     @todo_item.destroy
     respond_to do |format|
-      format.html { redirect_to todo_items_url, notice: 'Todo item was successfully destroyed.' }
+      format.html { redirect_to todo_lists, notice: 'Todo item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -64,7 +67,12 @@ class TodoItemsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_todo_item
-      @todo_item = TodoItem.find(params[:id])
+      @todo_item = @todo_list.todo_items.find(params[:id])
+    end
+
+    def set_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
+      #@todo_list = TodoList.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
